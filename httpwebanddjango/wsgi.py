f81aec2a13ce5d6de505597d84e760e4087a6a43
Tadas Sasnauskas <tadas@yoyo.lt>
2016-08-25 10:57:39 +0100

    Misc: rename HttpWebAndDjango to httpwebanddjango

--- httpwebanddjango/wsgi.py
+++ httpwebanddjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HttpWebAndDjango project.
+WSGI config for httpwebanddjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HttpWebAndDjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "httpwebanddjango.settings")
 
 application = get_wsgi_application()
